4c7fe9151d22281916f390da37fd9ba2493e3830
Tony Brierly <anthonylbrierly@gmail.com>
2025-10-20 13:11:53 -0700

    Debug Auth Cookie

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,8 @@ import { setupCors } from "./setup/cors"
 import { stripeRouter } from "./routes/stripe"
 
 const app = express()
+app.set("trust proxy", 1); // needed if behind Render's proxy
+console.log(env.NODE_ENV === "production"? " Production Mode": "Development Mode")
 
 setupSession(app)
 setupCors(app)
