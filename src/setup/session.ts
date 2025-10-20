e3782bd1aae922b76281f1ca06d7cabc3707de0c
Tony Brierly <anthonylbrierly@gmail.com>
2025-10-20 14:06:14 -0700

    More debugging

--- src/setup/session.ts
+++ src/setup/session.ts
@@ -10,17 +10,39 @@ declare module "express-session" {
   }
 }
 
+// export function setupSession(app: Express) {
+//   app.use(
+//     session({
+//       name: "sid",
+//       secret: env.SESSION_SECRET,
+//       cookie: {
+//         httpOnly: true,
+//         secure: env.NODE_ENV === "production",
+//         sameSite: "none",
+//         maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+//       },
+//       resave: true,
+//       saveUninitialized: true,
+//       store: new PrismaSessionStore(new PrismaClient(), {
+//         checkPeriod: 2 * 60 * 1000, //ms
+//         dbRecordIdIsSessionId: true,
+//         dbRecordIdFunction: undefined,
+//       }),
+//     })
+//   )
+// }
+
 export function setupSession(app: Express) {
   app.use(
     session({
-      // name: "sid",
+      name: "sid",
       secret: env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
+      resave: true,
+      saveUninitialized: true,
       cookie: {
         httpOnly: true,
         secure: env.NODE_ENV === "production",
-        sameSite: "none",
+        sameSite: env.NODE_ENV === "production" ? "none": "lax",
         maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
       },
       store: new PrismaSessionStore(new PrismaClient(), {
